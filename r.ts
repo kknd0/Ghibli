a79faa3ed67a2600c1af55f01284cd056333250c
apsu Li <ppaokamkl@gmail.com>
2025-05-08 17:52:50 -0700

    a

--- r.ts
+++ r.ts
@@ -1,12 +1,16 @@
 import { PutObjectCommand } from '@aws-sdk/client-s3'
-import { r2client } from './r2.js'
-
+import { r2 } from 'better-upload/server/helpers'
 const url =
   'https://storage.theapi.app/image/gen-26c1c535-9b8a-4986-af98-0d02f207c456.png'
+const r2client = r2({
+  accountId: process.env.CF_ACCOUNT_ID!,
+  accessKeyId: process.env.CF_ACCESS_KEY_ID!,
+  secretAccessKey: process.env.CF_SECRET_ACCESS_KEY!,
+})
 
 const response = await fetch(url)
 const fileBuffer = await response.arrayBuffer()
-const name = '3.png'
+const name = '4.png'
 
 const command = new PutObjectCommand({
   Bucket: 'apsu',
